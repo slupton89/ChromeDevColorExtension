1534f540d1a305f0757c2786c432ffa167f106b4
Shane Lupton <slupton89@icloud.com>
2019-02-18 21:32:30 -0700

    added popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+let color = document.getElementById('changeColor')
+
+chrome.storage.sync.get('color', (data) => {
+  changeColor.style.backgroundColor = data.color
+  changeColor.setAttribute('value', data.color)
+} )
